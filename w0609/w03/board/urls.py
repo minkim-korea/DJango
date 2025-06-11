657e3faecd6086083c12b45da9c18671b83fc59a
minkim-korea <km24778@gmail.com>
2025-06-11 18:31:06 +0900

    0611 ajax

--- w0609/w03/board/urls.py
+++ w0609/w03/board/urls.py
@@ -4,5 +4,6 @@ from django.urls import path,include
 app_name="board"
 urlpatterns = [
     path('list/',views.list ,name='list'),
-    path('view/',views.view ,name='view'),
+    path('view/',views.view ,name='view'),  # 1.form데이터 전송
+    path('view2/',views.view2 ,name='view2'), # 4.ajax 데이터전송 
 ]
