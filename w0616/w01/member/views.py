51009b64ea87dc2e9e72bec340a826e431ded477
minkim-korea <km24778@gmail.com>
2025-06-16 16:20:47 +0900

    회원가입부분

---
+++ w0616/w01/member/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django. http import JsonResponse
+from member.models import Member
+#id중복확인
+def idchk(request):
+    id= request.POST.get('id','')
+    print("넘어온 id : ",id)
+  ## db확인 - member테이블
+    try : 
+        qs = Member.objects.get(id=id)
+        print(qs)
+        result = 1 #아이디 존재함
+    except:
+        result= 0  # 아이디 존재하지않음 
+      
+    print("확인ㅣ",result)
+    context = {'result':result}
+    return JsonResponse(context)
+  
+
+
+def step04(request):
+    return render(request,"member/step04.html")
+
+
+def step03(request):
+    return render(request,"member/step03.html")
+
+
+def step02(request):
+    return render(request,"member/step02.html")
+
+
+
+def step01(request):
+    return render(request,"member/step01.html")
