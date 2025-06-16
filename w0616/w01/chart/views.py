dedf1a40b8bb5251aa1f2775c278b5c86651dc4a
minkim-korea <km24778@gmail.com>
2025-06-16 18:27:17 +0900

    차트그리기

--- w0616/w01/chart/views.py
+++ w0616/w01/chart/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from chart.models import TotalSales
 
 # 차트페이지 호출
-def chlist(request): 
+def chlist(request):
     profit =[19,20,21,22,23,24]
-    #profit =[20,15,7,25,27,30]
-    qs =  TotalSales.objects.filter(year=2025)
-    print(qs)  #  타입 QuerySet list 타입
-    print(list(qs.values()))  #타입 list 타입 
-    context ={'profit':profit,"list":qs,"list_list":list(qs.values())}
-    print(profit)
-
+    #profit = [20, 15, 7, 25, 27, 30]           # 타입 : List타입
+    qs = TotalSales.objects.filter(year=2025)
+    print('qs 기본구문 : ',qs)                  # 타입 : QuerySet List타입
+    print('list타입 구문 : ',list(qs.values())) # 타입 : List타입
+    context = {'profit':profit,'list':qs,'list_list':list(qs.values())}
+    print("영업이익 : ",profit)
     return render(request,'chart/chlist.html',context)
+  
